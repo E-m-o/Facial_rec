8c8fe05b902b6732b001713a8cf5a5613ecf20ca
E-m-o <vineet.biscuit@gmail.com>
2021-02-17 12:47:59 +0530

    use multiple casscade classifiers

--- face_detection.py
+++ face_detection.py
@@ -1,25 +1,47 @@
-import numpy as np
 import cv2
 
-face_cascade = cv2.CascadeClassifier("cascades/data/haarcascade_frontalface_alt2.xml")
-
+face_cascade_front = cv2.CascadeClassifier("cascades/data/haarcascade_frontalface_alt2.xml")
+face_cascade_profile = cv2.CascadeClassifier("cascades/data/haarcascade_profileface.xml")
+face_cascade_eyes = cv2.CascadeClassifier("cascades/data/haarcascade_eye.xml")
 cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+    faces_front = face_cascade_front.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+    faces_profile = face_cascade_profile.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+    faces_eyes = face_cascade_eyes.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+    for (x, y, w, h) in faces_front:
+        print(x, y, w, h)
 
-    for (x,y,w,h) in faces:
-        print(x,y,w,h)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = frame[y:y + h, x:x + w]
 
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = frame[y:y+h, x:x+w]
+        rec_color = (255, 0, 0)  # BGR format 0-255
+        stroke = 2
+        end_coord_x = x + w
+        end_coord_y = y + h
+        cv2.rectangle(frame, (x, y), (end_coord_x, end_coord_y), rec_color, stroke)
 
-        img_item = 'my-image.png'
-        cv2.imwrite(img_item, roi_gray)
+    for (x, y, w, h) in faces_profile:
+        print(x, y, w, h)
 
-        rec_color = (255, 0, 0) # BGR format 0-255
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = frame[y:y + h, x:x + w]
+
+        rec_color = (0, 255, 0)  # BGR format 0-255
+        stroke = 2
+        end_coord_x = x + w
+        end_coord_y = y + h
+        cv2.rectangle(frame, (x, y), (end_coord_x, end_coord_y), rec_color, stroke)
+
+    for (x, y, w, h) in faces_eyes:
+        print(x, y, w, h)
+
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = frame[y:y + h, x:x + w]
+
+        rec_color = (0, 0, 255)  # BGR format 0-255
         stroke = 2
         end_coord_x = x + w
         end_coord_y = y + h
@@ -31,3 +53,4 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
+
