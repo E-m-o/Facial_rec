b2077ae84830bfe4d9e9e2041b0071ac33f6f2bd
E-m-o <vineet.biscuit@gmail.com>
2021-02-19 17:00:18 +0530

    add video and update files

--- face_detection.py
+++ face_detection.py
@@ -3,16 +3,16 @@ import cv2
 face_cascade_front = cv2.CascadeClassifier("cascades/data/haarcascade_frontalface_alt2.xml")
 face_cascade_profile = cv2.CascadeClassifier("cascades/data/haarcascade_profileface.xml")
 face_cascade_eyes = cv2.CascadeClassifier("cascades/data/haarcascade_eye.xml")
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('test.avi')
 
-while True:
+while cap.isOpened():
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces_front = face_cascade_front.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
     faces_profile = face_cascade_profile.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
     faces_eyes = face_cascade_eyes.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
     for (x, y, w, h) in faces_front:
-        print(x, y, w, h)
+        # print(x, y, w, h)
 
         roi_gray = gray[y:y + h, x:x + w]
         roi_color = frame[y:y + h, x:x + w]
@@ -24,7 +24,7 @@ while True:
         cv2.rectangle(frame, (x, y), (end_coord_x, end_coord_y), rec_color, stroke)
 
     for (x, y, w, h) in faces_profile:
-        print(x, y, w, h)
+        # print(x, y, w, h)
 
         roi_gray = gray[y:y + h, x:x + w]
         roi_color = frame[y:y + h, x:x + w]
